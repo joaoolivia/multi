45fa33c676257e760b2df1045d4301b3d7819248
joaoolivia <58187985+joaoolivia@users.noreply.github.com>
2023-06-10 12:27:31 +0100

    Update a1.py

--- MULTI/a1.py
+++ MULTI/a1.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-dw = 100
+dw = 600
 
-x0 = 1030
+x0 = 1000
 H = 6500
 
 
@@ -29,23 +29,30 @@ H = 6500
 #
 #
 def model(dw):
-    heights = np.linspace(2000,12000,num=50)
+    heights = np.linspace(5000,20000,num=50)
     c = list()
     for h in heights:
-        angles = np.linspace(0.0001, np.arctan((dw/2)/h), num=50)
+        angles = np.linspace(0, np.arctan((dw/2)/h), num=50)
         t = list()
+        r = list()
         for a in angles:
             delta_t = (h)/(3E8*np.cos(a))-(h)/(3E8)
             t.append(delta_t)
-        p2 = np.poly1d(np.polyfit(angles, t, 2))
+            r.append(h*np.tan(a))
+       
+        p2 = np.poly1d(np.polyfit(r, t, 2))
         c.append(p2[2])
-    return(c,heights)
+     
+    return(c,heights,t,angles)
 
 
-c,heights = model(dw)
+c,heights,t,angles = model(dw)
+
+
 X = list()
 for h in heights:
     X.append(x0*np.exp(-(h)/(H)))
 
+#plt.plot(heights,c)
 plt.plot(X,c)
 plt.show()
